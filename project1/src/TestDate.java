1501911ad468e1a2f450f0f5329047b9fae89d73
PierrotZS <takimiyamizu@gmail.com>
2025-08-02 14:52:35 +0700

    Java: Date function and ModernDate function

---
+++ project1/src/TestDate.java
@@ -0,0 +1,22 @@
+public class TestDate {
+    public static void main(String[] args) {
+        //create object d1 call method in Class Date
+        Date d1 = new Date(2025, 8, 2);
+        // System.out.println(d1.toString());
+        System.out.println(d1); //call toString() method
+
+        // ไว้แซทค่าทีละตัว
+        d1.setYear(2024);
+        d1.setMonth(12);
+        d1.setDay(23);
+        System.out.println(d1); //call toString() method
+
+        //แสดงผลทีละตัว
+        System.out.println("Year is " + d1.getYear());
+        System.out.println("Month is " + d1.getMonth());
+
+        //เซทค่าใหม่ทั้งหมด
+        d1.setDate(2023, 1, 25);
+        System.out.println(d1); //call toString() method
+    }
+}
